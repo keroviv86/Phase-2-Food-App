08ccff3b0f7bdcb993b2225c2c999c347320cda3
Vivian Yim <keroviv86@gmail.com>
2022-05-20 16:11:21 -0500

    changed photo

--- food-app/src/components/HomePage.js
+++ food-app/src/components/HomePage.js
@@ -6,7 +6,7 @@ function HomePage() {
       <>
         <body className="homepage">
            <div className="container">
-             <img src="https://www.yimmyadventures.com/wp-content/uploads/2021/05/20200501-DSC_1917-scaled.jpg" alt=""/>
+             <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Flovepik.com%2Fimage-500816234%2Fjapanese-food-sashimi.html&psig=AOvVaw3nZ80n9KAKByNJLU56mulp&ust=1653167446427000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPiE7On-7vcCFQAAAAAdAAAAABAJ" alt=""/>
              <img src="https://www.chopstickchronicles.com/wp-content/uploads/2020/06/Tempura-Udon-update-18.jpg" alt=""/>
              <img src="https://www.sugarsaltmagic.com/wp-content/uploads/2020/11/Karaage-Chicken-Japanese-Fried-Chicken-13FEAT.jpg" alt=""/>
            </div>
